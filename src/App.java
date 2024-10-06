b294f7653187dc630b2c1acd0a0d9abcae401129
josetin_22 <josetin2001@gmail.com>
2024-10-06 15:22:24 +0530

    Added game window

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int boardWidth = 750;
+        int boardHeight = 250;
+
+        JFrame frame = new JFrame("chrome dino");
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
