3c606b3babe36fa85ba5c4f0b05bda599b9c0bce
josetin21 <josetin2001@gmail.com>
2024-10-07 22:27:20 +0530

    Sucessfully created v1.0

--- src/ChromeDinosaur.java
+++ src/ChromeDinosaur.java
@@ -31,19 +31,35 @@ public class ChromeDinosaur extends JPanel implements ActionListener,KeyListener
         }
     }
 
+    Block dinosaur;
+    Block cactus;
+
     //dinosaur
     int dinosaurWidth = 88;
     int dinosaurHeight = 94; 
     int dinosaurX = 50;
     int dinosaurY = boardHeight - dinosaurHeight;
 
+    //cactus
+    int cactus1Width = 34;
+    int cactus2Width = 69;
+    int cactus3Width = 102;    
+
+    int cactusHeight = 70;
+    int cactusX = 700;
+    int cactusY = boardHeight - cactusHeight;
+    ArrayList<Block> cactusArray;
+
     //physics
+    int velocityX = -12;
     int velocityY = 0; // dino jump speed
     int gravity = 1;
 
-    Block dinosaur;
-
+    boolean gameOver = false;
+    int score = 0;
+     
     Timer gameLoop;
+    Timer placeCactusTimer;
 
 
     public ChromeDinosaur(){
@@ -62,18 +78,69 @@ public class ChromeDinosaur extends JPanel implements ActionListener,KeyListener
         //dinosaur
         dinosaur = new Block(dinosaurX,dinosaurY, dinosaurWidth, dinosaurHeight, dinosaurImg);
 
+        //cactus
+        cactusArray = new ArrayList<Block>();
+
         //game timer
         gameLoop = new Timer(1000/60, this);
         gameLoop.start();
 
+        //placecactus timer
+        placeCactusTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                placeCactus();
+            }
+        });
+        placeCactusTimer.start(); 
     }
+    void placeCactus(){
+        if (gameOver) {
+            return;
+        }
+        double placeCactusChance = Math.random();//0-0.9999
+        if(placeCactusChance > .90){ //10% chance
+            Block cactus = new Block(cactusX, cactusY, cactus3Width, cactusHeight, cactus3Img);
+            cactusArray.add(cactus);
+        }
+        else if(placeCactusChance > .70){//20% chance
+            Block cactus = new Block(cactusX, cactusY, cactus2Width, cactusHeight, cactus2Img);
+            cactusArray.add(cactus);
+        }
+        else if(placeCactusChance > .50){//20% chance
+            Block cactus = new Block(cactusX, cactusY, cactus1Width, cactusHeight, cactus1Img);
+            cactusArray.add(cactus);
+        }
+        
+        if(cactusArray.size() > 10){
+            cactusArray.remove(0); //remove the first cactus from ArrayList
+        }
+    }
+
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         draw(g);
     }
 
     public void draw(Graphics g){
+        //dinosaur
         g.drawImage(dinosaur.img, dinosaur.x, dinosaur.y, dinosaur.width, dinosaur.height, null );
+
+        //catus
+        for(int i=0; i < cactusArray.size(); i++){
+            Block cactus = cactusArray.get(i);
+            g.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height, null);
+        }
+
+        //score 
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Courier",Font.PLAIN,32));
+        if (gameOver) {
+            g.drawString("Game Over:" + String.valueOf(score),10,35);
+        }
+        else{
+            g.drawString(String.valueOf(score),10,35);
+        }
     }
 
     public void move(){
@@ -84,13 +151,39 @@ public class ChromeDinosaur extends JPanel implements ActionListener,KeyListener
         if(dinosaur.y > dinosaurY){
             dinosaur.y = dinosaurY;
             velocityY = 0;
+            dinosaur.img = dinosaurImg;
         }
+
+        //cactus
+        for(int i=0; i < cactusArray.size(); i++){
+            Block cactus = cactusArray.get(i);
+            cactus.x += velocityX; 
+
+            if (collision(dinosaur, cactus)){
+                gameOver = true;
+                dinosaur.img = dinosaurDeadImg;
+            }
+        }
+
+        //score
+        score++;
+    }
+
+    boolean collision(Block a ,Block b){
+        return a.x < b.x + b.width && // a's top left corner doesn't reach b's top right corner
+               a.x + a.width > b.x && // a's top right corner passes b's top left corner
+               a.y < b.y + b.height && // a's top left corner doesn't reach b's bottom left corner
+               a.y + a.height > b.y; // a's bottom left corner passes b's top left corner
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         move();
         repaint();
+        if(gameOver){
+            placeCactusTimer.stop();
+            gameLoop.stop();
+        }
     }
 
     @Override
@@ -99,6 +192,17 @@ public class ChromeDinosaur extends JPanel implements ActionListener,KeyListener
             if(dinosaur.y == dinosaurY){
             // System.out.println("J");
             velocityY = -17;
+            dinosaur.img = dinosaurJumpImg;
+            }
+            if (gameOver) {
+                dinosaur.y = dinosaurY;
+                dinosaur.img = dinosaurImg;
+                velocityY = 0;
+                cactusArray.clear();
+                score = 0;
+                gameOver = false;
+                gameLoop.start();;
+                placeCactusTimer.start();
             }
         }
     }
